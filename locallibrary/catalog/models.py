75de45935d21fa8e7eb67c697bfeae40bac648a2
Andrew Ehrenberg <ehrena@umich.edu>
2019-02-05 03:49:12 +0000

    finished users HW

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+from django.contrib.auth.models import User
+from datetime import date
+
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -39,6 +42,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -61,6 +65,12 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
-
-from catalog.models import Book, Author, BookInstance, Genre
-
-from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
@@ -19,6 +17,17 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 def index(request):
     """View function for home page of site."""
@@ -36,7 +45,7 @@ def index(request):
     # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-   
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
