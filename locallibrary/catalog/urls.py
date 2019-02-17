54d64b65fc3dbfdee7a302d9015cef53d083cc78
Andrew Ehrenberg <ehrena@umich.edu>
2019-02-17 23:37:43 +0000

    book renewal form and permissions

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -28,3 +28,11 @@ urlpatterns += [
 urlpatterns += [
     path('borrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
 ]
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
